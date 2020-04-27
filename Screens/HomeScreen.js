64985596118d7d40c998453956ab758200be2fb1
UditTarini <udit.tarini937@gmail.com>
2020-04-28 00:21:39 +0530

    Newspaper icons added

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -24,16 +24,16 @@ export default class HomeScreen extends React.Component{
     render(){
       
       const topics = new Array();
-      topics[0] = new Array ( "Top Headlines", require("../assets/icons/trending.png") );
-      topics[1] = new Array ( "International", require("../assets/icons/businessman.png") );
-      topics[2] = new Array ( "Technology", require("../assets/icons/automation.png") );
-      topics[3] = new Array ( "Business", require("../assets/icons/teamwork.png") );
-      topics[4] = new Array ( "Sports", require("../assets/icons/soccer.png") );
-      topics[5] = new Array ( "Science", require("../assets/icons/molecule.png") );
-      topics[6] = new Array ( "Entertainment", require("../assets/icons/theater.png") );
-      topics[7] = new Array ( "Fashion", require("../assets/icons/designers.png") );
-      topics[8] = new Array ( "Travel", require("../assets/icons/globe.png") );
-      topics[9] = new Array ( "Lifestyle", require("../assets/icons/healthy.png") );
+      topics[0] = new Array ( "Top Headlines", require("../assets/TopicsIcons/trending.png") );
+      topics[1] = new Array ( "International", require("../assets/TopicsIcons/businessman.png") );
+      topics[2] = new Array ( "Technology", require("../assets/TopicsIcons/automation.png") );
+      topics[3] = new Array ( "Business", require("../assets/TopicsIcons/teamwork.png") );
+      topics[4] = new Array ( "Sports", require("../assets/TopicsIcons/soccer.png") );
+      topics[5] = new Array ( "Science", require("../assets/TopicsIcons/molecule.png") );
+      topics[6] = new Array ( "Entertainment", require("../assets/TopicsIcons/theater.png") );
+      topics[7] = new Array ( "Fashion", require("../assets/TopicsIcons/designers.png") );
+      topics[8] = new Array ( "Travel", require("../assets/TopicsIcons/globe.png") );
+      topics[9] = new Array ( "Lifestyle", require("../assets/TopicsIcons/healthy.png") );
     
       
       
--- Screens/NewspaperScreen.js
+++ Screens/NewspaperScreen.js
@@ -6,7 +6,49 @@ import { StyleSheet, Text,TouchableOpacity, Image,ScrollView,FlatList, View, Saf
 
 
 export default class NewspaperScreen extends React.Component {
- 
+  constructor(props){
+    super(props)
+    this.state={
+        source:" "
+     }
+  }
+  setSource = async (source)=> {
+      
+    await this.setState({source})
+    
+    this.props.navigation.navigate("NewzGeekz",{source:this.state.source})
+  }
+  
+  
+  
+    render() {
+      
+         const topics = new Array();
+         topics[0] = new Array("The Hindu", require("../assets/NewsIcons/TH,jpg"));
+         topics[1] = new Array("Times Of India", require("../assets/NewsIcons/TOI.png"));
+         topics[2] = new Array("The Indian Express", require("../assets/NewsIcons/IndExp.jpg"));
+         topics[3] = new Array("News18", require("../assets/NewsIcons/News18.jpg"));
+         topics[4] = new Array("The Quint", require("../assets/NewsIcons/quint.png"));
+         topics[5] = new Array("The Hindustan Times", require("../assets/NewsIcons/HT.jpg"));
+         
+         topics[6] = new Array("Al Jazeera", require("../assets/NewsIcons/Alzazeera.png"));
+         topics[7] = new Array("BBC", require("../assets/NewsIcons/BBCE.png"));
+         topics[8] = new Array("New York Tmes", require("../assets/NewsIcons/NYT.png"));
+         topics[9] = new Array("Wall Street Journal", require("../assets/NewsIcons/WSJ.jpg"));
+         topics[10] = new Array("BuzzFeed", require("../assets/NewsIcons/BuzzFeed.png"));
+         
+         topics[11] = new Array("Tech Chrunch", require("../assets/NewsIcons/TechChrunch.png"));
+         topics[12] = new Array("The Verge", require("../assets/NewsIcons/TheVerge.png"));
+         topics[13] = new Array("GSM Arena", require("../assets/NewsIcons/Gsm.png"));
+         topics[14] = new Array("Wired", require("../assets/NewsIcons/wired.png"));
+         topics[15] = new Array("The Next Web", require("../assets/NewsIcons/TNW.png"));
+         
+         topics[16] = new Array("Bloomberge", require("../assets/NewsIcons/bloom.png"));
+         topics[17] = new Array("Money Control", require("../assets/NewsIcons/moneycontrol.jpg"));
+         topics[18] = new Array("The Economic Times", require("../assets/NewsIcons/ET.jpg"));
+        
+      
+        topics
       return (
 
             
